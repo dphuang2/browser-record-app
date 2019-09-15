a4136bb10d6b55e76ebdfc128c1b8a99e8ab7d92
Dylan Huang <dylan.p.huang@gmail.com>
2019-09-15 15:14:20 -0700

    Added quick instruction to replay watching

--- utils/constants.js
+++ utils/constants.js
@@ -2,8 +2,9 @@ export const APP_NAME = 'Rewind';
 export const HERO_TITLE = 'Success! 🎉';
 export const HERO_MESSAGE = `${APP_NAME} has been successfully installed.
  ${APP_NAME} will immediately start collecting customer interaction data and
- displaying it here. Test it out yourself by navigating to your shop. Enjoy
- and please leave a review!`;
+ displaying it here. Test it out yourself by navigating to your shop and
+ watching your replay. To watch a replay, simply click on it. Enjoy and please
+ leave a review!`;
 export const SEEN_HERO_COOKIE_KEY = 'seenHero';
 export const JSON_WEB_TOKEN_COOKIE_KEY = 'token';
 export const TEST_SHOP_DOMAIN = 'test-rewind-app.myshopify.com';
